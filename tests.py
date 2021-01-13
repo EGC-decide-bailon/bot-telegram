46a142ee430b8b8ccb7adfb46a2aba1f050c50e2
pabmundel <55798790+pabmundel@users.noreply.github.com>
2021-01-13 11:33:48 +0100

    Añadido prueba unitaria incompleta

--- tests.py
+++ tests.py
@@ -11,3 +11,9 @@ class TestMethods(unittest.TestCase):
         credentials = {"username": "user", "password":"rinoceronte2"}
         r = requests.post(URL_GW + 'authentication/login' + credentials)
         self.assertEqual(r.status_code,200)
+
+    def login_test_fail(self):
+        credentials = {"username": "user", "password":"rinoceronte"}
+        r = requests.post(URL_GW + 'authentication/login' + credentials)
+        print(json.load(r.text))
+        self.assertEqual(r.text,'')
