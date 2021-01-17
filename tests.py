d1708d0e78c0e9705a302dd2cf8b5532d893760f
pabmundel <55798790+pabmundel@users.noreply.github.com>
2021-01-17 12:11:46 +0100

    Creado test unitario save_vote

--- tests.py
+++ tests.py
@@ -28,3 +28,18 @@ class TestMethods(unittest.TestCase):
         data = {'token': token}
         r = requests.post(URL_GW + 'authentication/getuser/', data)
         self.assertEqual(r.status_code,200)
+
+    def save_vote(self):
+        credentials = {"username": "user", "password":"rinoceronte2"}
+        token = llamadas.get_token(credentials)
+        data = {'token': token}
+        user = requests.post(URL_GW + 'authentication/getuser/', data)
+        data_dict = {
+            "vote": { "a": 0,"b": 1},
+            "voting": 3,
+            "voter": user['id'],
+            "token": token}
+        headers = {"Authorization":"Token   " + token,
+            "Content-Type": "application/json"}
+        r = requests.post(config.BASE_URL_HEROKU + "store/", json=data_dict, headers = headers)
+        self.assertEqual(r.status_code, 200)
