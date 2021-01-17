3014bd1be1bc733be9f504c586f8a884d6e6824c
pabmundel <55798790+pabmundel@users.noreply.github.com>
2021-01-17 13:07:15 +0100

    Editado nombre test

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
-import requests
 import unittest
+import requests
 import json
 from bot import llamadas
 
@@ -8,40 +8,41 @@ URL_GW = URL_BASE + 'gateway/'
 
 class TestMethods(unittest.TestCase):
 
-    def login_test(self):
+    def test_login_test(self):
         credentials = {"username": "user", "password":"rinoceronte2"}
         r = requests.post(URL_GW + 'authentication/login' + credentials)
         self.assertEqual(r.status_code,200)
 
-    def get_votings(self):
+    def test_get_votings(self):
         r = requests.get(URL_GW + 'voting/?id=' + id_votacion)
         self.assertEqual(r.status_code,200)
 
-    def get_voting(self):
+    def test_get_voting(self):
         id_votacion = 1
         r = requests.get(URL_GW + 'voting/?id=' + id_votacion)
         self.assertEqual(r.status_code,200)
 
-    def get_user(self):
+    def test_get_user(self):
         credentials = {"username": "user", "password":"rinoceronte2"}
         token = llamadas.get_token(credentials)
         data = {'token': token}
         r = requests.post(URL_GW + 'authentication/getuser/', data)
         self.assertEqual(r.status_code,200)
 
-    def save_vote(self):
+    def test_save_vote(self):
         credentials = {"username": "user", "password":"rinoceronte2"}
         token = llamadas.get_token(credentials)
         data = {'token': token}
         user = requests.post(URL_GW + 'authentication/getuser/', data)
+        id = user['id']
         data_dict = {
             "vote": { "a": 0,"b": 1},
             "voting": 3,
-            "voter": user['id'],
+            "voter": id,
             "token": token}
         headers = {"Authorization":"Token " + token,
             "Content-Type": "application/json"}
-        r = requests.post(config.BASE_URL_HEROKU + "store/", json=data_dict, headers = headers)
+        r = requests.post(config.URL_BASE + "store/", json=data_dict, headers = headers)
         self.assertEqual(r.status_code, 200)
 
     #Test de errores
