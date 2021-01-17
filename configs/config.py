2d7cfcda6366c31e61a8c2ba21d03afd85fb5f8e
pabmundel <55798790+pabmundel@users.noreply.github.com>
2021-01-17 23:13:06 +0100

    Editado variables y gitignore

--- configs/config.py
+++ configs/config.py
@@ -1,13 +1,12 @@
+import os
+
 #=======DECIDE============#
 URL_BASE = 'https://decide-voting.herokuapp.com/'
 API_DECIDE = URL_BASE + 'gateway/'
-
-#=======TELEGRAM==========#
-BOT_TOKEN = '1448420544:AAGLjmcgg5AF4cgJ_y8sVKrU6-9yES417rg'
 
 #=======HEROKU============#
 WEBHOOK = True
 IP = '0.0.0.0'
 PORT = '8443'
-BOT_URL = 'https://api.telegram.org/bot1448420544:AAGLjmcgg5AF4cgJ_y8sVKrU6-9yES417rg'
+BOT_URL = 'https://api.telegram.org/bot' + os.environ['BOT_TOKEN']
 HEROKU_APP_NAME = 'bot-slack-decide-bailon'
