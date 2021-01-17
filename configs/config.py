a708d3b49a0a1cadd80724e88f9dbeb08dade9a7
pabmundel <55798790+pabmundel@users.noreply.github.com>
2021-01-17 23:17:17 +0100

    Editado key del bot

--- configs/config.py
+++ configs/config.py
@@ -4,9 +4,11 @@ import os
 URL_BASE = 'https://decide-voting.herokuapp.com/'
 API_DECIDE = URL_BASE + 'gateway/'
 
+BOT_TOKEN = os.environ['BOT_TOKEN']
+
 #=======HEROKU============#
 WEBHOOK = True
 IP = '0.0.0.0'
 PORT = '8443'
-BOT_URL = 'https://api.telegram.org/bot' + os.environ['BOT_TOKEN']
+BOT_URL = 'https://api.telegram.org/bot' + BOT_TOKEN
 HEROKU_APP_NAME = 'bot-slack-decide-bailon'
